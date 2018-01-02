19247a98460aa8e6e5238e17461a7c0f3a7b2349
Ryo Ota <nwtgck@gmail.com>
2018-01-03 00:44:14 +0900

    [change] Change Main to generate demo1.svg

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ cd <this repo>
 stack build
 stack exec diagrams-rubiks-cube-prac-exe
 ```
+
+## Genrated image
+
+![demo1](demo_images/demo1.svg)
--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ module Main where
 import qualified Diagrams.RubiksCube
 import qualified Diagrams.RubiksCube.Move as RubiksCube.Move
 import qualified Diagrams.RubiksCube.Model
+import qualified Data.Colour.RGBSpace
 
 import Control.Lens hiding ((#))
 import qualified Diagrams.Prelude
@@ -17,12 +18,12 @@ import qualified Diagrams.Backend.SVG
 
 main :: IO ()
 main = do
-  let 
-      c   = Diagrams.RubiksCube.solvedRubiksCube ^. Diagrams.RubiksCube.Model.undoMoves [RubiksCube.Move.R, RubiksCube.Move.U, RubiksCube.Move.R', RubiksCube.Move.U']
+  let cube :: Diagrams.RubiksCube.Model.RubiksCube (Data.Colour.RGBSpace.Colour Double)
+      cube    = Diagrams.RubiksCube.solvedRubiksCube ^. Diagrams.RubiksCube.Model.undoMoves [RubiksCube.Move.R, RubiksCube.Move.U, RubiksCube.Move.R', RubiksCube.Move.U']
       settings :: Diagrams.RubiksCube.MovesSettings Double
       settings = (Diagrams.Prelude.with & Diagrams.RubiksCube.showStart .~ True)
       d :: Diagrams.Prelude.Diagram (Diagrams.Backend.SVG.B)
       -- d   = drawMoves settings solvedRubiksCube []
-      d   = Diagrams.RubiksCube.drawRubiksCube Diagrams.Prelude.def Diagrams.RubiksCube.solvedRubiksCube
-  Diagrams.Backend.SVG.renderSVG "hoge.svg" (Diagrams.Prelude.mkWidth 250) d
+      d   = Diagrams.RubiksCube.drawRubiksCube Diagrams.Prelude.def cube
+  Diagrams.Backend.SVG.renderSVG "demo_images/demo1.svg" (Diagrams.Prelude.mkWidth 250) d
   return ()
